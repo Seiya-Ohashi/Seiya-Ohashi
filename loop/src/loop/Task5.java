bf95835497beb82744ac98db1582ab67e2dc34c6
Seiya-Ohashi <lalala0400@gmail.com>
2020-03-06 17:51:12 +0900

    課題提出 - ループ

--- loop/src/loop/Task5.java
+++ loop/src/loop/Task5.java
@@ -2,15 +2,15 @@ package loop;
 
 public class Task5 {
 	public static void main(String[] args) {
-		for (int step = 1; step <= 9 ; step++) {
-			System.out.println();
-			for (int num = 1; num <= 9 ; num++) {
-				if(step * num / 10 < 1){
+		for (int step = 1; step <= 9; step++) {
+			for (int num = 1; num <= 9; num++) {
+				if(step * num < 10){
 					System.out.print("  " + step * num);
 				}else{
-					System.out.print(" " +step * num);
+					System.out.print(" " + step * num);
 				}
 			}
+			System.out.println();
 		}
 	}
 }
