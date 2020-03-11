ace315000728c697da4a5ac22a87d5a946a03621
Seiya-Ohashi <lalala0400@gmail.com>
2020-03-11 15:19:05 +0900

    課題提出 - インスタンス＆クラス

---
+++ instance/src/instance/Task2.java
@@ -0,0 +1,9 @@
+package instance;
+
+public class Task2 {
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.name = "大橋 晟也";
+		person.age = 29;
+	}
+}
---
+++ instance/src/instance/Task3.java
@@ -0,0 +1,10 @@
+package instance;
+
+public class Task3 {
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.name = "大橋 晟也";
+		person.age = 29;
+		person.myProfile();
+	}
+}
