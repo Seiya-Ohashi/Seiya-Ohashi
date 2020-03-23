968520eb7bb5f4028dd3d6d2955cb1e88c0adce3
Seiya-Ohashi <lalala0400@gmail.com>
2020-03-23 18:13:17 +0900

    再提出 - 配列

--- array/src/array/Task3.java
+++ array/src/array/Task3.java
@@ -5,27 +5,26 @@ import java.util.Random;
 public class Task3 {
 	public static void main(String[] args) {
 		int max = 0;
-		int min = 0;
+		int min = 9;								//最小値0が確定になるのを防止
 		int[] numbers = new int[6];
 		Random random = new Random();
 		System.out.print("[");
 
 		for(int i = 0; i < numbers.length; i++){	//配列にランダムな値を格納
-			if(i == 0){
-				numbers[i] = random.nextInt(9);
-				min = numbers[i];					//最小値0が確定になるのを防止
-			}else{
-				numbers[i] = random.nextInt(9);
-			}if(numbers[i] > max){
+			numbers[i] = random.nextInt(10);
+			if(numbers[i] > max){
 				max = numbers[i];					//最大値判定
 			}else if(numbers[i] < min){
 				min = numbers[i];					//最小値判定
 			}
 		}
-		for(int j = 0; j < numbers.length -1 ; j++){
-			System.out.print(numbers[j] + ",");		//要素5個カンマ区切りで表示
+		for(int j = 0; j <= numbers.length -1 ; j++){
+			if(j < numbers.length -1){
+				System.out.print(numbers[j] + ",");		//要素5個カンマ区切りで表示
+			}else{
+				System.out.println(numbers[j] + "]");
+			}
 		}
-		System.out.println(numbers[5] + "]");
 		System.out.println("最大値: " + max);
 		System.out.println("最小値: " + min);
 	}
