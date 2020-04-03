217959b9d98484a3b50fcdfd37ae13010a566698
Seiya-Ohashi <lalala0400@gmail.com>
2020-04-03 17:11:37 +0900

    再提出 - オブジェクト指向

--- object/src/object/Person.java
+++ object/src/object/Person.java
@@ -25,11 +25,11 @@ public class Person {
 		}
 
 	//コンストラクタ 引数なし
-	Person(){
+	public Person(){
 	}
 
 	//コンストラクタ 引数あり
-	Person(String name, int age, int wallet){
+	public Person(String name, int age, int wallet){
 		this.name = name;
 		this.age = age;
 		this.wallet = wallet;
@@ -39,20 +39,5 @@ public class Person {
 		System.out.println("私の名前は" + this.name + "です。");
 		System.out.println("私の年齢は" + this.age + "です。");
 		System.out.println("よろしくお願いします。");
-	}
-	public static void main(String[] args) {
-		System.out.println("インスタンスを生成していない状態");
-		System.out.println(Person.wallet);
-
-		//インスタンスを２つ生成
-		Person hoge01 = new Person("大橋晟也", 29, 1000);
-		Person hoge02 = new Person("田中太郎", 22, 2000);
-
-		System.out.println("インスタンスhoge01でwalletにアクセス");
-		System.out.println(hoge01.wallet);
-		System.out.println("インスタンスhoge02でwalletにアクセス");
-		System.out.println(hoge02.wallet);
-		System.out.println("クラスPersonでwalletにアクセス");
-		System.out.println(Person.wallet);
 	}
 }
