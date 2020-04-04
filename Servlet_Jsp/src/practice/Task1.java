6e900471d7de6f4cf3b650ddbc3e871435105610
Seiya-Ohashi <lalala0400@gmail.com>
2020-04-04 16:24:01 +0900

    課題提出 - サーブレットとJSP

--- Servlet_Jsp/src/practice/Task1.java
+++ Servlet_Jsp/src/practice/Task1.java
@@ -30,5 +30,5 @@ public class Task1 extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
+//
 }
